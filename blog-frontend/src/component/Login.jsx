fb07415dacf77cfb1980f4c3e089380e21bb9e1d
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-20 19:17:14 +0530

    state for input value added

--- blog-frontend/src/component/Login.jsx
+++ blog-frontend/src/component/Login.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+
 export default function Login(){
+    const [nameInput,setNameInput]=useState('')
+    const [passInput,setPassInput]=useState('')
     return (
         <>
-        <div >
-        <input type="text" placeholder="username"></input>
-        <input type="text" placeholder="password"></input>
-        <button className="w-full" onClick={()=>{
+        <div className="w-screen h-screen flex flex-col justify-center  gap-4 items-center " >
+            <p className="text-5xl">Login</p>
+            <div className="flex flex-col gap-4">
+            <input type="text" placeholder="username" className=" border-2 border-black focus:outline-none px-10 py-2"
+            value={nameInput}
+            onChange={(e)=>setNameInput(e.target.value)}
+            ></input>
+        <input type="text" placeholder="password" className=" border-2 border-black focus:outline-none px-10 py-2"
+          value={passInput}
+          onChange={(e)=>setPassInput(e.target.value)}
+        ></input>
+        <button className="px-10 border-2 border-black" onClick={()=>{
             alert('submitted the form')
         }}>submit</button>
+            </div>
+      
         </div>
         </>
     )
--- blog-frontend/src/component/Register.jsx
+++ blog-frontend/src/component/Register.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 export default function Register(){
+    
+    const [nameInput,setNameInput]=useState('')
+    const [passInput,setPassInput]=useState('')
     return (
         <>
-        <div className="h-screen w-screen  flex flex-col justify-center items-center" >
-        <input type="text" placeholder="username"></input>
-        <input type="text" placeholder="password"></input>
-        <button className="w-3/4 border-2 border-black" onClick={()=>{
+        <div className="w-screen h-screen flex flex-col justify-center  gap-4 items-center " >
+            <p className="text-5xl">register</p>
+            <div className="flex flex-col gap-4">
+            <input type="text" placeholder="username" className=" border-2 border-black focus:outline-none px-10 py-2"
+            value={nameInput}
+            onChange={(e)=>setNameInput(e.target.value)}
+            ></input>
+        <input type="text" placeholder="password" className=" border-2 border-black focus:outline-none px-10 py-2"
+          value={passInput}
+          onChange={(e)=>setPassInput(e.target.value)}
+        ></input>
+        <button className="px-10 border-2 border-black" onClick={()=>{
             alert('submitted the form')
         }}>submit</button>
+            </div>
+      
         </div>
         </>
     )
